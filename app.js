54940d6d46c6f3846707790921fcba21da5c3011
JuniorTechie-coder <jtanmay787@gmail.com>
2026-08-29 13:29:33 +0530

    chore: clean backend and add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const pool = require('./db');
 const userRoutes = require('./routes/users');
 const workspacesRoutes = require('./routes/workspaces');
@@ -13,12 +14,14 @@ const app = express();
 
 //MiddleWare
 app.use(express.json());
+app.use(cors());
 
 //Logger
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 });
+
 
 
 //Routes
